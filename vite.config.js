e00d1e565d46d44f2a6d289501e26b8697e78cb2
cyt9999 <chi_tsai@cmoney.com.tw>
2026-01-06 10:08:12 +0800

    Set base path for GitHub repository in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 新增這一行，填入你的 GitHub 儲存庫名稱，前後都要有斜線
+  base: '/investment-talk-2025/',
 })
