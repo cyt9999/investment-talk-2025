7d2e6d771833e240e9ad3f70c01fc607cec5fb84
cyt9999 <chi_tsai@cmoney.com.tw>
2026-01-20 14:23:08 +0800

    netifly adjust

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // 新增這一行，填入你的 GitHub 儲存庫名稱，前後都要有斜線
-  base: '/investment-talk-2025/',
+  base: '/',
 })
